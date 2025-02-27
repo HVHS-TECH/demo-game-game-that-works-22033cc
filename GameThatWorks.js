cc8fe113653a80d9ebfbfb03379cd6a9a91f150a
22033cc <22033cc@hvhs.school.nz>
2025-02-27 21:34:32 +0000

    standardizing coin deaths

--- GameThatWorks.js
+++ GameThatWorks.js
@@ -4,15 +4,17 @@ const canvasWidth = 500;
 const playerSize = 50;
 const playerStart = 200;
 const coinSize = 20;
-const TimeOut = 1500;
+const timeOut = 5000;
+const warning = 2000;
 var score = 0;
 var LifeExpectancy = 5000; 
 var playerSpeed = 3;
-
+var gameState = "start"
+var i =0;
 function setup(){
     console.log("playter")
     cnv = new Canvas (canvasWidth,canvasHeight)
-    player = new Sprite (playerStart, playerStart, playerSize, playerSize,)
+    player = new Sprite (playerStart, playerStart, playerSize, playerSize,'k')
     player.color = 'green';
 	totalCoin = new Group();
 } 
@@ -21,20 +23,28 @@ function draw(){
 	text("score:"+score, 20, 20)
     background('skyblue')
     playerMovement()
+	if (totalCoin.length<10){
 	if (random(0,1000)<20){
 		createCoin()
 		console.log(random(0,1000))
 	}
+} else{
+	console.log("limit reached")
+}
+	
     text("score = "+score, 50,50)
-
+	for ( i=0; i<totalCoin.length;i++){
+		checkCoinTime(totalCoin[i])
+	}
+	score =0;
+/*0
 	for (var i = 0; i < totalCoin.length; i++){
 		// Check Coin time should return true if the coin is old and needs to be deleted
 		checkCoinTime(totalCoin[i])
 		console.log("psychedelic breakfast")
 		console.log(totalCoin.length)
 	}
-		
-
+	*/
 totalCoin.collides(player,addPoint);
 }
 
@@ -48,11 +58,19 @@ function createCoin(){
 	coin=new Sprite (random(0, canvasHeight), random(0, canvasWidth), coinSize);
 	coin.color = 'yellow';
     coin.spawntime = millis();
+	console.log("spawntime "+coin.spawntime );
 	totalCoin.add(coin);
 }
 
 
 function checkCoinTime(){
+	
+
+	if (coin.spawntime +timeOut < millis()){
+		console.log("cranberry sauce "+totalCoin[i])
+		coin.remove()
+	}
+	/*
 	console.log("timeoutstarted")
     // Check if the coin has been around too long (COIN_TIMEPUT millisecomnds)
     if (coin.spawntime + TimeOut < millis()){
@@ -61,10 +79,16 @@ function checkCoinTime(){
         coin.remove()
 		console.log("coin killed")
 		
-    } else{
+    } else if (coin.spawntime+TimeOut+ warning < millis()) {
+		console.log("WARNING! WARNING!")
+		coin.color = "red"
+	} else {
 		console.log("cornflakes")
 	}
+		*/
+
 }
+
 function playerMovement(){
 	if (kb.pressing('a')) {
 	player.vel.x = -playerSpeed;
