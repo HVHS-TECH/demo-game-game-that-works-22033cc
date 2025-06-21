b1f173aabb55223d2a93a04f645504c6612824d8
22033cc <22033cc@hvhs.school.nz>
2025-06-21 07:55:08 +0000

    working on game

--- GameThatWorks.js
+++ GameThatWorks.js
@@ -6,28 +6,45 @@ const playerStart = 200;
 const coinSize = 20;
 const timeOut = 5000;
 const warning = 2000;
+var Speed = 3;
+const controls = ['w','a','s','d','k','l'];
 var score = 0;
 var LifeExpectancy = 5000; 
 var playerSpeed = 3;
 var gameState = "start"
 var i =0;
 var lives = 3;
+var nextSpeedUp = 5;
+
+
 
 function setup(){
-    console.log("playter");
+    console.log("player");
     cnv = new Canvas (canvasWidth,canvasHeight);
+	displayMode('centered')
+	background("pink")
     player = new Sprite (playerStart, playerStart, playerSize, playerSize,'k');
     player.color = 'green';
 	totalCoin = new Group();
 } 
 
 function draw(){
+	if (gameState=="start"){
+		
+		text("game that works",100,50)
+		text("press Enter to start", 100,250)
+		if (kb.pressed('enter')){
+			gameState="running";
+		}
+	}
 	if (gameState=="running"){ 
-		text("score:"+score, 20, 20)
+		
+
     	background('skyblue')
     	playerMovement()
+	
 		if (totalCoin.length<10){
-			if (random(0,1000)<20){
+			if (random(0,1000)<15){
 			createCoin()
 			console.log(random(0,1000))
 		}
@@ -36,12 +53,35 @@ function draw(){
 	}
 
     	text("score = "+score, 50,50)
+		text("lives = "+lives, 50,60)
 	for ( i=0; i<totalCoin.length;i++){
 		checkCoinTime(totalCoin[i])
 	}
 		totalCoin.collides(player,addPoint);
+		if (score == nextSpeedUp)
+		if (lives < 0 ){
+			gameState ="dead";
+		}
+		if (score == nextSpeedUp){
+			score = score+1;
+			nextSpeedUp = nextSpeedUp+5;
+			if (nextSpeedUp == 35){
+				nextSpeedup = 0;
+				
+			}
+		if (nextSpeedUp == 0){
+			text("MAX SPEED!",10,10)
+		}
+		}
 	} else if (gameState=="dead"){
-		endGame();
+		text("uh,oh! you failed to collect all the coins!", 50,50)
+		text("you scored"+score, 50, 50)
+		text("press enter to replay");
+		if(kb.pressed('enter')){
+			score = 0;
+			lives = 3;
+			gameState = playing;
+		}
 	}
 }
 
@@ -55,7 +95,11 @@ function addPoint(_coin_,_player_) {
 
 function createCoin(){
 	//random (0, WIDNOWHEIGHT)
-	coin=new Sprite (random(0, canvasHeight), random(0, canvasWidth), coinSize);
+	var coinSpawnY = (Math.random()*canvasWidth)
+	var coinSpawnX = (Math.random()*canvasHeight)
+	console.log(coinSpawnY)
+	console.log(coinSpawnX)
+	coin=new Sprite (coinSpawnX, coinSpawnY, coinSize);
 	coin.color = 'yellow';
     coin.spawntime = millis();
 	console.log("spawntime "+coin.spawntime );
@@ -69,6 +113,8 @@ function checkCoinTime(checkingCoin){
 		
 		console.log("A coin has died of dementia")
 		checkingCoin.remove()
+		
+		lives = lives-1;
 	} else if (coin.spawntime+warning < millis()){
 		console.log("cranberry sauce")
 		checkingCoin.color = 'red';
@@ -78,40 +124,36 @@ function checkCoinTime(checkingCoin){
 }
 
 function playerMovement(){
-	if (kb.pressing('a')) {
-	player.vel.x = -playerSpeed;
-	}
-	
-	else if (kb.pressing ('d')) {
-	player.vel.x = playerSpeed;
-	
-	};
-	if (kb.released('a')) {
-		player.vel.x = 0;
-	}else if (kb.released('d')){
-		player.vel.x = 0;
-	}
-	
-	if (kb.pressing('s')) {
-		player.vel.y = playerSpeed;
-		}
-		
-		else if (kb.pressing ('w')) {
-		player.vel.y = -playerSpeed;
-		
-		}
-	if (kb.released('a')) {
-			player.vel.x = 0;
-	}else if (kb.released('d')){
-			player.vel.x = 0;
-		}
-    if (kb.released('w')){
-        player.vel.y = 0;
+	var xPress=0;
+    var yPress=0;
+    /* controls for x*/
+    if (kb.pressing(controls[1])) {
+        //left
+        xPress= xPress-Speed;
+    };
+     
+     if (kb.pressing (controls[3])) {
+        //right
+        xPress = xPress+Speed;
+    };
+    /* controls for y*/
+    if (kb.pressing(controls[0])) {
+        //up
+        yPress= yPress-Speed;
+    };
+        
+     if (kb.pressing (controls[2])) {
+        //down
+        yPress = yPress+Speed;
 
-    }else if (kb.released('s')){
-        player.vel.y = 0;
-    }
+    };
+ 
+
+    player.vel.x = xPress;
+    player.vel.y = yPress;
+
 }
+
 function endGame(){
 
 }
